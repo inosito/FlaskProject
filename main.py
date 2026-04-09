8096d9e87a5107f6338cbaf403cd66fe20ee90ee
Micheal Walsh <mwalsh.05@outlook.com>
2026-04-09 12:58:30 -0230

    new routes

--- main.py
+++ main.py
@@ -10,7 +10,15 @@ Session(app)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("home.html")
+
+@app.route('/view')
+def view():
+    return render_template("view.html")
+
+@app.route('/edit')
+def edit():
+    return render_template("edit.html")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
