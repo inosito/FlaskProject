8ad71fae3cf497aa247e797353b9b19595a796f1
Micheal Walsh <mwalsh.05@outlook.com>
2026-04-09 11:19:08 -0230

    templates

--- main.py
+++ main.py
@@ -8,5 +8,9 @@ app.config['SESSION_TYPE'] = "filesystem"
 
 Session(app)
 
+@app.route('/')
+def index():
+    return render_template("index.html")
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
